3498311910ffc306b3ca8398f31b8da4adbe230b
Sett Teta <stetanonsakul@gmail.com>
2023-03-10 13:51:03 +0700

    change links to main and protect admin page

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,16 +1,17 @@
 import Header from 'components/header'
 import Footer from 'components/footer'
 import * as React from 'react'
-
 import VideoCard from 'components/Card.js'
 import EditCard from 'components/editCard'
-
 import Head from 'next/head'
 import { useState } from "react";
 import Link from 'next/link'
 import { useForm } from "react-hook-form";
+import { useSession } from "next-auth/react"
 
 export default function AdminPage({ videos }) {
+
+    const { data: session } = useSession()
 
     const [videosToShow, setVideosToShow] = useState(6);
     const [searchValue, setSearchValue] = useState("");
@@ -209,112 +210,115 @@ export default function AdminPage({ videos }) {
         window.location.reload(true);
     }
 
-    return (
-        <main role="main">
-            <div>
-                <Head>
-                    <title>VR Tours</title>
-                    <meta name="description" content="A VR Video Hosting platform" />
-                </Head>
-            </div>
-
-            <Header />
-
-            <section className="jumbotron text-center" style={{ marginTop: "10vh" }}>
-                <div className="container">
-                    <br></br>
-                    <h1 className="jumbotron-heading">VR Videos just for you</h1>
-                    <p className="lead text-muted">
-                        Browse our collection of VR videos and view them with your full entertainment
-                    </p>
+    if (session) {
+        return (
+            <main role="main">
+                <div>
+                    <Head>
+                        <title>VR Tours</title>
+                        <meta name="description" content="A VR Video Hosting platform" />
+                    </Head>
                 </div>
-                <Link href="/admin/add">Go to add page</Link>
-                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">Add</button>
-            </section>
 
-            <br />
+                <Header />
 
-            <div className="input-group rounded" style={{ padding: "0 15% 0 15%" }}>
-                <input type="search" className="form-control rounded" placeholder="Thailand" aria-label="Search" aria-describedby="search-addon" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-                <span className="input-group-text border-0" id="search-addon">
-                    <i className="fas fa-search" onClick={search}>Clear</i>
-                </span>
-            </div>
+                <section className="jumbotron text-center" style={{ marginTop: "10vh" }}>
+                    <div className="container">
+                        <br></br>
+                        <h1 className="jumbotron-heading">VR Videos just for you</h1>
+                        <p className="lead text-muted">
+                            Browse our collection of VR videos and view them with your full entertainment
+                        </p>
+                    </div>
+                    <Link href="/admin/add">Go to add page</Link>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addModal">Add</button>
+                </section>
 
-            <br />
+                <br />
 
-            <div className="album py-5 bg-light">
-                <div className="container-xxl content-row">
-                    <div className="row">
-                        {renderVideoCards()}
-                        {videosToShow < videos.length && (
-                            <button className="btn btn-secondary" onClick={loadMoreVideos}>Load more</button>
-                        )}
+                <div className="input-group rounded" style={{ padding: "0 15% 0 15%" }}>
+                    <input type="search" className="form-control rounded" placeholder="Thailand" aria-label="Search" aria-describedby="search-addon" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                    <span className="input-group-text border-0" id="search-addon">
+                        <i className="fas fa-search" onClick={search}>Clear</i>
+                    </span>
+                </div>
+
+                <br />
+
+                <div className="album py-5 bg-light">
+                    <div className="container-xxl content-row">
+                        <div className="row">
+                            {renderVideoCards()}
+                            {videosToShow < videos.length && (
+                                <button className="btn btn-secondary" onClick={loadMoreVideos}>Load more</button>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
 
 
 
-            <div className="modal fade" id="addModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Add Video</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div className="modal fade" id="addModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Add Video</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <form onSubmit={handleSubmit(addVideo)}>
+
+                                    <div className="mb-3">
+                                        <label htmlFor="title" className="col-form-label">Title:</label>
+                                        <input type="text" className="form-control" id="title" {...register("title", { required: false })} placeholder="Video Title" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="link" className="col-form-label">URL Link:</label>
+                                        <input className="form-control" id="link" {...register("link", { required: false })} placeholder="URL of Video"></input>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="thumbnail" className="col-form-label">Thumbnail:</label>
+                                        <input className="form-control" id="thumbnail" {...register("thumbnail", { required: false })} placeholder="URL of Video"></input>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="desc" className="col-form-label">Description:</label>
+                                        <input className="form-control" id="desc" {...register("desc", { required: false })} placeholder="URL of Video"></input>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="duration" className="col-form-label">Duration:</label>
+                                        <input className="form-control" id="duration" {...register("duration", { required: false })} placeholder="URL of Video"></input>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="location" className="col-form-label">Location:</label>
+                                        <input className="form-control" id="location" {...register("location", { required: false })} placeholder="URL of Video"></input>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="dateOfUpload" className="col-form-label">Date of Upload:</label>
+                                        <input className="form-control" id="dateOfUpload" {...register("dateOfUpload", { required: false })} placeholder="URL of Video"></input>
+                                    </div>
+
+
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" className="btn btn-primary">Submit</button>
+                                    </div>
+
+                                </form>
+                            </div>
+
                         </div>
-                        <div className="modal-body">
-                            <form onSubmit={handleSubmit(addVideo)}>
-
-                                <div className="mb-3">
-                                    <label htmlFor="title" className="col-form-label">Title:</label>
-                                    <input type="text" className="form-control" id="title" {...register("title", { required: false })} placeholder="Video Title" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="link" className="col-form-label">URL Link:</label>
-                                    <input className="form-control" id="link" {...register("link", { required: false })} placeholder="URL of Video"></input>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="thumbnail" className="col-form-label">Thumbnail:</label>
-                                    <input className="form-control" id="thumbnail" {...register("thumbnail", { required: false })} placeholder="URL of Video"></input>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="desc" className="col-form-label">Description:</label>
-                                    <input className="form-control" id="desc" {...register("desc", { required: false })} placeholder="URL of Video"></input>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="duration" className="col-form-label">Duration:</label>
-                                    <input className="form-control" id="duration" {...register("duration", { required: false })} placeholder="URL of Video"></input>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="location" className="col-form-label">Location:</label>
-                                    <input className="form-control" id="location" {...register("location", { required: false })} placeholder="URL of Video"></input>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="dateOfUpload" className="col-form-label">Date of Upload:</label>
-                                    <input className="form-control" id="dateOfUpload" {...register("dateOfUpload", { required: false })} placeholder="URL of Video"></input>
-                                </div>
-
-
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" className="btn btn-primary">Submit</button>
-                                </div>
-
-                            </form>
-                        </div>
-
                     </div>
                 </div>
-            </div>
 
-            <Footer />
-        </main>
-    )
+                <Footer />
+            </main>
+        )
+    }  return <p>Access Denied</p>
+
 }
 
 export async function getServerSideProps() {
-    const res = await fetch(`https://auvr-git-admin-cms-settteta.vercel.app/api/browse/videos`)
+    const res = await fetch(`https://auvr.vercel.app/api/browse/videos`)
     const videos = await res.json()
     console.debug('blog 1', videos)
     return { props: { videos } }
--- pages/browse/[id].jsx
+++ pages/browse/[id].jsx
@@ -126,7 +126,7 @@ export default function Video({ video }) {
 // STEP 1: This function will be executed at the server before loading the page.
 export async function getServerSideProps({ params }) {
     console.debug('params', params)
-    const res = await fetch(`https://auvr-git-admin-cms-settteta.vercel.app/api/browse/videos/${params.id}`)
+    const res = await fetch(`https://auvr.vercel.app/api/browse/videos/${params.id}`)
     // const res = await fetch(`http://localhost:3000/api/browse/videos/${params.id}`)
     const video = await res.json()
     console.debug('blog 1', video)
