dc104d9ded058cbaa56925b97a6d5ca4bce86273
Sett Teta <stetanonsakul@gmail.com>
2023-03-23 13:22:43 +0700

    fix 6

--- pages/api/browse/categories/[id].js
+++ pages/api/browse/categories/[id].js
@@ -1,9 +1,11 @@
 // pages/api/categories/[id].js
 import { connect } from "mongoose";
 import Category from "../../../../models/category";
+import * as Mongoose from 'mongoose'
 
 const connectionString = process.env.MONGODB_URI;
-set('strictQuery', false);
+
+Mongoose.set('strictQuery', false);
 
 export default async function handler(req, res) {
     await connect(connectionString);
--- pages/api/browse/categories/index.js
+++ pages/api/browse/categories/index.js
@@ -1,9 +1,11 @@
 // pages/api/categories/index.js
 import { connect, set } from "mongoose";
 import Category from "../../../../models/category";
+import * as Mongoose from 'mongoose'
 
 const connectionString = process.env.MONGODB_URI;
-set('strictQuery', false);
+
+Mongoose.set('strictQuery', false);
 
 export default async function handler(req, res) {
     await connect(connectionString);
