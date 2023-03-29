12f5fe71bdcc40ac915b559e317d90ff6d42b339
Sett Teta <stetanonsakul@gmail.com>
2023-03-29 13:32:39 +0700

    loader make fix

--- components/loader.js
+++ components/loader.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Loader() {
     return (
-        <div style={{display:"flex", position:"absolute", top:"-57px", left:"6.5px", height:"0", zIndex:"10000000000"}}>
+        <div style={{display:"flex", position:"fixed", top:"-57px", left:"6.5px", height:"0", zIndex:"10000000000"}}>
             <Image src={loadIcon} alt="asd" style={{display:"inline-block", margin:"auto", width:"80px"}}/>
         </div>
     )
